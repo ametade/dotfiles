6dd43a5149fa0730933c5bcc8e227fcde9b98104
Rodrigo Pinheiro <ametade@gmail.com>
2013-03-08 16:48:36 +0000

    Installed nerdtree

--- bin/fabfile.py
+++ bin/fabfile.py
@@ -4,7 +4,7 @@ from fabric.api import *
 # Use this then the server cannot make outside connections
 
 def deploy():
-    with lcd(".."):
+    with lcd("../vim"):
         local('tar czf /tmp/vim.tar.gz vimrc autoload backup bundle syntax tmp')
         put('/tmp/vim.tar.gz', '/tmp')
 
